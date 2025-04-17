0e2b5c7049bdb15f5b89589f9d6e4cf67c7bafc4
Leonardo Balbino <leovinicius42@outlook.com>
2025-04-17 19:53:58 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+function Home() {
+
+  return <>
+    <h1>
+    Escrevo diante da janela aberta.
+    Minha caneta é cor das venezianas:
+    💚 Verde!... E que leves, lindas filigranas
+    Desenha o sol na página deserta!
+
+    Não sei que paisagista doidivanas
+    Mistura os tons... acerta... desacerta...
+    Sempre em busca de nova descoberta,
+    Vai colorindo as horas quotidianas...
+
+    🍃 Jogos da luz dançando na folhagem!
+    Do que eu ia escrever até me esqueço...
+    Para quê pensar? Também sou da paisagem...
+
+    Vago, solúvel no ar, fico sonhando...
+    E me transmuto... iriso-me... estremeço...
+    Nos leves dedos que me vão pintando! 💚
+    </h1>
+  </>
+}
+
+export default Home;
